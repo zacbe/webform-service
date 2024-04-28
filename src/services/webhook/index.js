dd64f5d434c6829734c018eef343ce147cb2d6a4
@zacbe <zacbegonzalez@gmail.com>
2024-04-28 18:51:35 +0200

    refactor webhook

--- src/services/webhook/index.js
+++ src/services/webhook/index.js
@@ -1,37 +1,48 @@
-import querystring from "querystring";
+import createHttpError from "http-errors";
 import { EventBridgeClient } from "@aws-sdk/client-eventbridge";
 import { composeRes } from "../../helpers/index.js";
+import { EBClient } from "../../libs/index.js";
+import { webformMiddleware as middleware } from "../../middleware/index.js";
+import { validateEmail } from "../../helpers/index.js";
 import {
   eventbridge,
   WEBHOOK_EVENT_TYPE,
   WEBHOOK_SOURCE,
+  EMAIL_ORIGIN,
 } from "../../config/index.js";
-import { EBClient } from "../../libs/index.js";
 
 const client = new EventBridgeClient(eventbridge.region);
 
 /**
  * @typedef {import('../../libs/index').EBClient}
  */
-export async function webhook(event) {
-  const { body = null } = event || {};
-  const parsedBody = querystring.parse(body);
-
+async function baseHandler(event) {
   try {
-    const { name, email, subject, message } = parsedBody;
-    const form = { name, email, subject, message };
+    const {
+      body,
+      pathParameters: { target },
+    } = event;
+
+    if (!validateEmail(target)) {
+      console.info("Invalid email format:", target);
+      throw new Error("invalid email");
+    }
+
     const eb = new EBClient(client, eventbridge.eventBusArn);
 
     const webhookEvent = {
       type: WEBHOOK_EVENT_TYPE,
       source: WEBHOOK_SOURCE,
-      data: form,
+      data: { target, origin: EMAIL_ORIGIN, form: body },
     };
 
     await eb.send(webhookEvent);
 
     return composeRes(204);
-  } catch (error) {
-    return composeRes(400, error);
+  } catch (e) {
+    throw createHttpError(400, e);
   }
 }
+
+const handler = middleware(baseHandler);
+export { handler };
