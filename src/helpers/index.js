ad933af37abdf31498e74aebee0a15324d18a420
@zacbe <zacbegonzalez@gmail.com>
2024-04-28 18:51:02 +0200

    refactor helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,12 +1,13 @@
 /**
  * Composes a response object with the given status code and optional body.
  *
+ * @function composeRes
  * @param {number} code - The HTTP status code for the response.
  * @param {object|null} [body] - The optional body content of the response.
  * @returns {object} The composed response object.
  * @throws {TypeError} Throws a TypeError if the code parameter is not a number.
  */
-function composeRes(code, body = null) {
+export function composeRes(code, body = null) {
   let responseBody = body;
 
   if (body instanceof Error) {
@@ -25,4 +26,21 @@ function composeRes(code, body = null) {
   };
 }
 
-export { composeRes };
+/**
+ * @function validateEmail
+ * @param {string} email
+ * @returns {Boolean}
+ */
+export function validateEmail(email) {
+  // Regular expression pattern for validating email
+  const regex = /^[\w.+%-]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return regex.test(email);
+}
+
+export function on(promise) {
+  return promise
+    .then((data) => {
+      return [null, data];
+    })
+    .catch((err) => [err]);
+}
